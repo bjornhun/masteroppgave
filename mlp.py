eede93b77d9c66e5a00bf9e51ce2f9cdb377bf7a
Bjornar Hunshamar <bjornarhuns@gmail.com>
2018-02-23 16:09:25 +0900

    Change variable names

--- mlp.py
+++ mlp.py
@@ -4,37 +4,36 @@ import pickle
 
 # Parameters
 learning_rate = 0.001
-training_epochs = 100
+n_epochs = 100
 batch_size = 100
-display_step = 1
 
 # Network Parameters
-n_input = 13*99 # Number of feature
+n_inputs = 13*99 # Number of feature
 n_hidden_1 = 512 # 1st layer number of features
 n_hidden_2 = 512 # 2nd layer number of features
 n_hidden_3 = 512 # 2nd layer number of features
 n_classes = 2 # Number of classes to predict
 
-X, Y = pickle.load(open("data/train.p", "rb"))
+X_train, y_train = pickle.load(open("data/train.p", "rb"))
 X_test, Y_test = pickle.load(open("data/test.p", "rb"))
 X_val, y_val = pickle.load(open("data/val.p", "rb"))
 
-n_train = len(X)
+n_train = len(X_train)
 n_test = len(X_test)
 n_val = len(X_val)
 
-X = X.reshape(n_train, n_input)
-X_test = X_test.reshape(n_test, n_input)
-X_val = X_val.reshape(n_val, n_input)
+X_train = X_train.reshape(n_train, n_inputs)
+X_test = X_test.reshape(n_test, n_inputs)
+X_val = X_val.reshape(n_val, n_inputs)
 
 # tf Graph input
-x = tf.placeholder("float", [None, n_input])
+X = tf.placeholder("float", [None, n_inputs])
 y = tf.placeholder("float", [None, n_classes])
 
 # Create model
-def multilayer_perceptron(x, weights, biases):
+def multilayer_perceptron(X, weights, biases):
     # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.add(tf.matmul(X, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
@@ -48,7 +47,7 @@ def multilayer_perceptron(x, weights, biases):
 
 # Store layers weight & bias
 weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h1': tf.Variable(tf.random_normal([n_inputs, n_hidden_1])),
     'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
     'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
     'out': tf.Variable(tf.random_normal([n_hidden_3, n_classes]))
@@ -62,7 +61,7 @@ biases = {
 }
 
 # Construct model
-pred = multilayer_perceptron(x, weights, biases)
+pred = multilayer_perceptron(X, weights, biases)
 
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
@@ -76,31 +75,30 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     # Training cycle
-    for epoch in range(training_epochs):
+    for epoch in range(n_epochs):
         avg_cost = 0.
-        total_batch = int(len(X)/batch_size)
-        X_batches = np.array_split(X, total_batch)
-        Y_batches = np.array_split(Y, total_batch)
+        total_batch = int(len(X_train)/batch_size)
+        X_batches = np.array_split(X_train, total_batch)
+        Y_batches = np.array_split(y_train, total_batch)
         # Loop over all batches
         for i in range(total_batch):
             batch_x, batch_y = X_batches[i], Y_batches[i]
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+            _, c = sess.run([optimizer, cost], feed_dict={X: batch_x,
                                                           y: batch_y})
             # Compute average loss
             avg_cost += c / total_batch
         # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-            # Test model
-            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-            # Calculate accuracy
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-            print("Accuracy:", accuracy.eval({x: X_val, y: y_val}))
+        print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print("Accuracy:", accuracy.eval({X: X_val, y: y_val}))
     print("Optimization Finished!")
 
     # Test model
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print("Accuracy:", accuracy.eval({x: X_test, y: Y_test}))
+    print("Accuracy:", accuracy.eval({X: X_test, y: Y_test}))
