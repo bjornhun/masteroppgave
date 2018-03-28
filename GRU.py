19e82fed010f783edd7985ff175f4ad024b79b60
Bjornar Hunshamar <bjornarhuns@gmail.com>
2018-03-28 18:52:41 +0200

    Change cell definition. Inexplicably improves training.

--- GRU.py
+++ GRU.py
@@ -24,44 +24,59 @@ X_batches = np.array_split(X_train, n_batches)
 Y_batches = np.array_split(y_train, n_batches)
 
 X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-y = tf.placeholder(tf.int32, [None])
+y = tf.placeholder(tf.int32, [None, n_outputs])
 keep_prob = tf.placeholder_with_default(1.0, shape=())
 
-cells = [tf.contrib.rnn.GRUCell(n_neurons) for layer in range(n_layers)]
+cells = []
+for _ in range(n_layers):
+    grucell = tf.contrib.rnn.GRUCell(n_neurons)
+    grucell = tf.contrib.rnn.DropoutWrapper(grucell, output_keep_prob=keep_prob)
+    cells.append(grucell)
+cell = tf.contrib.rnn.MultiRNNCell(cells)
+outputs, states = tf.nn.dynamic_rnn(cell=cell, inputs=X, dtype=tf.float32)
+'''cells = [tf.contrib.rnn.GRUCell(n_neurons) for layer in range(n_layers)]
 #cells_proj = [tf.contrib.rnn.OutputProjectionWrapper(cell, output_size=n_outputs, activation=tf.nn.softmax) for cell in cells]
 cells_drop = [tf.contrib.rnn.DropoutWrapper(cell, input_keep_prob=keep_prob) for cell in cells]
 multi_layer_cell = tf.contrib.rnn.MultiRNNCell(cells_drop)
-outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)
+outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)'''
 
 stacked_rnn_outputs = tf.reshape(outputs, [-1, n_neurons])
-stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs, activation=tf.nn.softmax)
+stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs)
 outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_outputs])
 
 logits = outputs[:,-1,:]
+#logits = tf.layers.dense(last, n_outputs)
+
+
+
 #logits = tf.layers.dense(outputs[:,-1,:], n_outputs, activation=tf.nn.softmax)
-xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+xentropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
 
 loss = tf.reduce_mean(xentropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 training_op = optimizer.minimize(loss)
-correct = tf.nn.in_top_k(logits, y, 1)
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+#correct = tf.nn.in_top_k(logits, y, 1)
+#accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
+        avg_loss = 0.
         for i in range(n_batches):
             X_batch, y_batch = X_batches[i], Y_batches[i]
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch, keep_prob: .5})
-        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
-        acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
-        log = logits.eval(feed_dict={X: X_test, y: y_test})
-        y_val = y.eval(feed_dict={X: X_test, y: y_test})
-        print(['%.4f' % elem for elem in log[:,1]][10:20])
-        print(y_val[10:20])
-        print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost), "Val accuracy:", accuracy.eval({X: X_val, y: y_val}))
+            _, c = sess.run([training_op, loss], feed_dict={X: X_batch,
+                                                            y: y_batch,
+                                                            keep_prob: 0.5})
+            avg_loss += c / n_batches            
+        #log = logits.eval(feed_dict={X: X_test, y: y_test})
+        #y_pred = y.eval(feed_dict={X: X_test, y: y_test})
+        #print(['%.4f' % elem for elem in log[:,1]][10:20])
+        #print(y_pred[10:20])
+        print("Epoch:", '%04d' % (epoch+1), "loss=", "{:.9f}".format(avg_loss), "Val accuracy:", accuracy.eval({X: X_val, y: y_val}))
     print("Optimization Finished!")
 
     print("Test accuracy:", accuracy.eval({X: X_test, y: y_test}))
