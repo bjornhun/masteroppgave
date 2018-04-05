b7d6bcd00c39fd4573e973e2db64d4b80ee8f109
Bjornar Hunshamar <bjornarhuns@gmail.com>
2018-04-05 12:50:54 +0200

    Works

--- realtime.py
+++ realtime.py
@@ -1,15 +1,19 @@
 from model import *
 import pyaudio
-import winsound
+import winsound 
+import time
+
+fs = 16000
+framesize = 4000
+seq_len = int(float(framesize)/float(fs)*100-1)
+threshold = 0.9999
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=fs, input=True, frames_per_buffer=framesize)
 
 def record():
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=16000)
-    data = stream.read(16000)
+    data = stream.read(framesize)
     raw_speech = np.fromstring(data, dtype=np.int16)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
     return normalize(mfcc(raw_speech, numcep=26))
 
 with tf.Session() as sess:
@@ -19,19 +23,25 @@ with tf.Session() as sess:
     probs = np.array([])
     st = np.zeros((n_layers, 1, n_neurons))
     while True:
+        rec_start = time.time()
         coeff = record()
-        st, wp = sess.run([states, wakeword_probs], feed_dict={X: [coeff], initial_state: st, seq_length : 99})
+        rec_end = time.time()
+        #print("Recording:", rec_end-rec_start)
+        start = time.time()
+        st, wp = sess.run([states, wakeword_probs], feed_dict={X: [coeff], initial_state: st, seq_length: seq_len})
         for val in wp:
             probs = np.append(probs, val)
 
-        probs = probs[-99:]
-        threshold = 0.99
+        probs = probs[-seq_len:]
         detections = probs > threshold
-        if cooldown == 0:
-            if True in detections:
-                count+=1
-                print("Wake word detected #", count)
-                winsound.Beep(1000, 300)
-                cooldown = 3
-        else:
-            cooldown -= 1
+        if True in detections:
+            count+=1
+            print("Wake word detected #", count)
+            #winsound.Beep(1000, 300)
+            st = np.zeros((n_layers, 1, n_neurons))
+        end = time.time()
+        #print("Processing:", end-start)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
